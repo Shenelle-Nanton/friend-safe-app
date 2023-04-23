68d06045ca01b3cf20982737cbcf06f6ecb18194
MatthewD-Dan <60522543+MatthewD-Dan@users.noreply.github.com>
2023-04-23 00:18:12 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -3,12 +3,12 @@ from flask_login import UserMixin
 from sqlalchemy.sql.expression import func
 from App.database import db
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    active = db.Column(db.Boolean, default=False)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -43,7 +43,7 @@ class Chat(db.Model):
         self.text = text
 
     def toggle(self):
-        self.done = not self.done
+        self.active = not self.active
         db.session.add(self)
         db.session.commit()
 
@@ -59,6 +59,16 @@ class Chat(db.Model):
 
     def __repr__(self):
         return f'<Chat: {self.id} | {self.user.username} | {self.text} | {"active" if self.done else "not active"} | categories{self.cat_list()}>'
+
+class ChatCategory(db.Model):
+    __tablename__ = 'chat_category'
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    last_modified = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+
+    def __repr__(self):
+        return f'<ChatCategory last modified {self.last_modified.strftime("%Y/%m/%d, %H:%m:%s")}>'
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -81,16 +91,6 @@ class Category(db.Model):
             "user": self.user.username,
             "text": self.text
         }
-
-class ChatCategory(db.Model):
-    __tablename__ = 'chat_category'
-    id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    last_modified = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
-
-    def __repr__(self):
-        return f'<ChatCategory last modified {self.last_modified.strftime("%Y/%m/%d, %H:%m:%s")}>'
 
 class RegularUser(User):
     __tablename__ = 'regular_user'
@@ -119,6 +119,13 @@ class RegularUser(User):
             db.session.add(chat)
             db.session.commit()
             return chat
+        return None
+
+    def toggle_chat(self, chat_id):
+        chat = Chat.query.filter_by(id=chat_id, user_id=self.id).first()
+        if chat:
+            chat.toggle()
+            return Chat
         return None
 
     def add_chat_category(self, chat_id, category_text):
@@ -157,7 +164,7 @@ class RegularUser(User):
         return f'<RegularUser {self.id} : {self.username} - {self.email}>'
 
 class Admin(User):
-    __tablename__ = 'Admin'
+    __tablename__ = 'admin'
     admin_id = db.Column(db.String(120), unique=True, nullable=False)
 
     def get_all_chats_json(self):
@@ -173,8 +180,10 @@ class Admin(User):
     def search_chats(self, q, active, page):
         matching_chats = None
         if q != "" and active == "any":
-            matching_chats = Chat.query.join(RegularUser).filter(
-                db.or_(RegularUser.username.ilike(f'%{q}%'), Chat.id.ilike(f'%{q}%')), Chat.done == is_active)
+            matching_chats = Chat.query.join(RegularUser).filter(db.or_(RegularUser.username.ilike(f'%{q}%'), Chat.id.ilike(f'%{q}%')))
+        elif q != "":
+            is_active = True if active == "true" else False
+            matching_chats = Chat.query.join(RegularUser).filter(db.or_(RegularUser.username.ilike(f'%{q}%'), Chat.text.ilike(f'%{q}%')), Chat.active == is_active)
         elif active != "any":
             is_active = True if active == "true" else False
             matching_chats = Chat.query.filter_by(active=is_active)
@@ -190,11 +199,8 @@ class Admin(User):
             )
         elif q != "any":
             is_active = True if active == "true" else False
-            matching_users = User.query.join(RegularUser).filter(
-                db.or_(RegularUser.username.ilike(f'%{q}%'), User.email.ilike(f'%{q}%'), User.id.ilike(f'%{q}%')),
-                User.active == is_active
-            )
-        elif active != "any":
+            matching_users = User.query.join(RegularUser).filter(db.or_(RegularUser.username.ilike(f'%{q}%'), User.email.ilike(f'%{q}%'), User.id.ilike(f'%{q}%')), User.active = is_active)
+        elif not active == "any":
             is_active = True if active == "true" else False
             matching_users = User.query.filter_by(
                 active = is_active
